44b639a2248f150a51379a56b99d38949c89367b
DRAKO-NERO <osadsuza@gmail.com>
2025-12-14 18:13:51 -0500

    Add files via upload

---
+++ wp-content/languages/loco/plugins/woocommerce-es_PE.l10n.php
@@ -0,0 +1,55 @@
+<?php
+return ['project-id-version'=>'WooCommerce 10.3.5','report-msgid-bugs-to'=>'https://wordpress.org/support/plugin/woocommerce','last-translator'=>'','language-team'=>'Español de Perú','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2025-11-12T13:17:51+00:00','po-revision-date'=>'2025-11-26 17:15+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'woocommerce','language'=>'es_PE','plural-forms'=>'nplurals=2; plural=n != 1;','x-loco-version'=>'2.8.0; wp-6.8.3; php-8.2.12','messages'=>['Add to cart'=>'añadir a la cesta
+','Name'=>'Nombre
+',' You don\'t have a subscription, <a href="%1$s" class="woocommerce-purchase-subscription">subscribe</a> to update.'=>'No tienes suscripción, suscríbete para actualizar.
+','Add to cart behaviour'=>'Añadir al carrito comportamiento
+','Redirect to the cart page after successful addition'=>'Redirigir a la página del carrito después de una adición exitosa
+','Enable AJAX add to cart buttons on archives'=>'Habilitar los botones AJAX para agregar al carrito en archivos
+','%1$s has been removed from your cart because it has since been modified. You can add it back to your cart <a href="%2$s">here</a>.'=>'%1$s ha sido eliminado de su carrito porque desde entonces ha sido modificado. Puedes agregarlo nuevamente a tu carrito <a href="%2$s">aquí</a>.
+','%d item from your previous order is currently unavailable and could not be added to your cart.'=>'El artículo %d de su pedido anterior no está disponible actualmente y no se pudo agregar a su carrito.
+' . "\0" . '','You cannot add another "%s" to your cart.'=>'No puedes agregar otro "%s" a tu carrito.
+','You cannot add &quot;%s&quot; to the cart because the product is out of stock.'=>'No puedes agregar "%s" al carrito porque el producto está agotado.
+','You cannot add that amount of &quot;%1$s&quot; to the cart because there is not enough stock (%2$s remaining).'=>'No puedes agregar esa cantidad de "%1$s" al carrito porque no hay suficiente stock (%2$s restantes).
+','You cannot add that amount to the cart &mdash; we have %1$s in stock and you already have %2$s in your cart.'=>'No puedes agregar esa cantidad al carrito — tenemos %1$s en stock y tú ya tienes %2$s en tu carrito.
+','Please choose the quantity of items you wish to add to your cart&hellip;'=>'Por favor, elige la cantidad de artículos que deseas añadir a tu carrito…
+','Please choose a product to add to your cart&hellip;'=>'Añadir al carrito comportamiento
+','Please select some product options before adding this product to your cart.'=>'Seleccione algunas opciones de productos antes de agregar este producto a su carrito.
+','Your selection has been reset. Please select some product options before adding this product to your cart.'=>'Su selección ha sido restablecida. Seleccione algunas opciones de productos antes de agregar este producto a su carrito.
+','Add to cart: &ldquo;%s&rdquo;'=>'Añadir al carrito: “%s”
+','&ldquo;%s&rdquo; has been added to your cart'=>'“%s” se ha añadido a tu carrito
+','Add to cart details.'=>'Añadir al carrito detalles.
+','Add to cart URL.'=>'Añadir a la URL del carrito.
+','Add to cart description.'=>'Añadir a la descripción del carrito.
+','Add to cart text.'=>'Añadir al texto del carrito.
+','Add to cart single text.'=>'Añadir al carrito texto único.
+','%s has been added to your cart.'=>'%s se ha añadido a tu carrito.
+' . "\0" . '','Create a unique URL that applies a discount and optionally adds one or more products to the customer\'s cart.'=>'Crea una URL única que aplique un descuento y opcionalmente agregue uno o más productos al carrito del cliente.
+','Subscribe'=>'Suscribirse
+','You don\'t have a subscription for <strong>%1$s</strong>. Subscribe to receive updates and streamlined support.'=>'No tienes una suscripción por %1$s. Suscríbete para recibir actualizaciones y soporte optimizado.
+','You don\'t have subscriptions for <strong>%1$s Woo extensions</strong>. Subscribe to receive updates and streamlined support.'=>'No tienes suscripciones para extensiones Woo de %1$. Suscríbete para recibir actualizaciones y soporte optimizado.
+','Add to Cart + Options'=>'Añadir al carrito + Opciones
+','The Add to Cart + Options templates allow defining a different layout for each product type.'=>'Las plantillas Agregar al carrito + Opciones permiten definir un diseño diferente para cada tipo de producto.
+','Please select a valid quantity to add to the cart.'=>'Seleccione una cantidad válida para agregar al carrito.
+','Please select some products to add to the cart.'=>'Seleccione algunos productos para agregar al carrito.
+','Please select product attributes before adding to cart.'=>'Seleccione los atributos del producto antes de agregarlo al carrito.
+','Added to cart'=>'Añadido al carrito
+','The provided checkout link was out of date or invalid. No products were added to the cart.'=>'El enlace de pago proporcionado estaba desactualizado o no era válido. No se agregaron productos al carrito.
+','Template nameExternal Product Add to Cart + Options'=>'Nombre de la plantilla Producto externo Añadir al carrito + Opciones
+','Template used to display the Add to Cart + Options form for External Products.'=>'Plantilla utilizada para mostrar el formulario Agregar al carrito + Opciones para productos externos.
+','Template nameGrouped Product Add to Cart + Options'=>'Nombre de la plantilla Producto agrupado Añadir al carrito + Opciones
+','Template used to display the Add to Cart + Options form for Grouped Products.'=>'Plantilla utilizada para mostrar el formulario Agregar al carrito + Opciones para productos agrupados.
+','Template nameSimple Product Add to Cart + Options'=>'Nombre de la plantilla Producto sencillo Añadir al carrito + Opciones
+','Template used to display the Add to Cart + Options form for Simple Products.'=>'Plantilla utilizada para mostrar el formulario Agregar al carrito + Opciones para productos simples.
+','Template nameVariable Product Add to Cart + Options'=>'Nombre de la plantilla Producto Variable Añadir al carrito + Opciones
+','Template used to display the Add to Cart + Options form for Variable Products.'=>'Plantilla utilizada para mostrar el formulario Agregar al carrito + Opciones para productos variables.
+','This variation will not trigger your customer\'s shipping calculator in cart or at checkout. This product also won\'t require your customers to enter their shipping details at checkout. <a href="https://woocommerce.com/document/managing-products/#adding-a-virtual-product" target="_blank" rel="noreferrer">Read more about virtual products</a>.'=>'Esta variación no activará la calculadora de envío de su cliente en el carrito ni al finalizar la compra. Este producto tampoco requerirá que sus clientes ingresen sus detalles de envío al finalizar la compra. Lea más sobre productos virtuales.
+','This product will not trigger your customer\'s shipping calculator in cart or at checkout. This product also won\'t require your customers to enter their shipping details at checkout. <a href="https://woocommerce.com/document/managing-products/#adding-a-virtual-product" target="_blank" rel="noreferrer">Read more about virtual products</a>.'=>'Este producto no activará la calculadora de envío de su cliente en el carrito ni al finalizar la compra. Este producto tampoco requerirá que sus clientes ingresen sus detalles de envío al finalizar la compra. Lea más sobre productos virtuales.
+','Back-in-stock notifications for this product are now being processed. Subscribed customers will receive these emails over the next few minutes. You can monitor or manage individual subscriptions on the <a href="%s">Stock Notifications page</a>.'=>'Actualmente se están procesando notificaciones de existencias de este producto. Los clientes suscritos recibirán estos correos electrónicos en los próximos minutos. Puede monitorear o administrar suscripciones individuales en la página Notificaciones de stock.
+','Quantity of this item to add to the cart.'=>'Cantidad de este artículo para añadir al carrito.
+','Does the product have additional options before it can be added to the cart?'=>'¿El producto tiene opciones adicionales antes de poder agregarlo al carrito?
+','Add to cart button parameters.'=>'Añadir al carrito parámetros del botón.
+','Subscribe to update'=>'Suscríbete para actualizar
+','Subscribe to %s'=>'Suscríbete a %s
+','Subscribe for $%s'=>'Suscríbete por $%s
+','To collect email and notify your subscribers, set up MailPoet.'=>'Para recopilar correos electrónicos y notificar a sus suscriptores, configure MailPoet.
+']];
